c43f551407c68c7dc5ef562d5fe0a8bc79c7c1ab
kiviaaraujobandtec <kivia.araujo@bandtec.com.br>
2022-01-11 21:19:13 -0300

    incluindo registros @kivia-code

--- CLIENTES.cob
+++ CLIENTES.cob
@@ -4,84 +4,124 @@
       *OBJETIVO: SISTEMA DE GESTAO DE CLIENTES
       *AUTHOR: KIVIAAL
       ****************************************
-       ENVIRONMENT DIVISION.
+           ENVIRONMENT DIVISION.
+           INPUT-OUTPUT SECTION.
+           FILE-CONTROL.
+             SELECT CLIENTES ASSIGN TO
+               'C:\Cobol2\CLIENTES.DAT'
+           ORGANIZATION IS INDEXED
+           ACCESS MODE IS RANDOM
+           FILE STATUS IS CLIENTES-STATUS
+           RECORD KEY  IS CLIENTES-CHAVE.
 
-       DATA DIVISION.
+           DATA DIVISION.
+           FILE SECTION.
+           FD CLIENTES.
+           01 CLIENTES-REG.
+               05 CLIENTES-CHAVE.
+                 10 CLIENTES-ID  PIC X(04).
+
+               05 CLIENTES-NOME  PIC X(30).
+               05 CLIENTES-EMAIL PIC X(40).
+
        WORKING-STORAGE SECTION.
        77 WRK-OPCAO  PIC X(1).
        77 WRK-MODULO PIC X(25).
-       77 WRK-TECLA  PIC X(1).
+       77 WRK-TECLA PIC X(1).
+       77 CLIENTES-STATUS PIC 9(2).
+
 
 
        SCREEN SECTION.
-       01  TELA.
-           05 LIMPA-TELA.
-              10 BLANK SCREEN.
-              10 LINE 01 COLUMN 01 PIC X(20) ERASE EOL
-              BACKGROUND-COLOR 03.
+       01 TELA.
+            05 LIMPA-TELA.
+                10 BLANK SCREEN.
+                10 LINE 01 COLUMN 01 PIC X(20) ERASE EOL
+                   BACKGROUND-COLOR 3.
+                10 LINE 01 COLUMN 25 PIC X(20)
+                   BACKGROUND-COLOR 3  FOREGROUND-COLOR 0
+                              FROM 'SISTEMA DE CLIENTES '.
+                10 LINE 02 COLUMN 01 PIC X(25) ERASE EOL
+                   BACKGROUND-COLOR 1 FROM WRK-MODULO.
+       01 MENU.
+            05 LINE 07 COLUMN 15 VALUE '1 - INCLUIR'.
+            05 LINE 08 COLUMN 15 VALUE '2 - CONSULTAR'.
+            05 LINE 09 COLUMN 15 VALUE '3 - ALTERAR'.
+            05 LINE 10 COLUMN 15 VALUE '4 - EXCLUIR'.
+            05 LINE 11 COLUMN 15 VALUE '5 - RELATORIO'.
+            05 LINE 12 COLUMN 15 VALUE 'X - SAIDA'.
+            05 LINE 13 COLUMN 15 VALUE 'OPCAO......: ' .
+            05 LINE 13 COLUMN 28 USING WRK-OPCAO.
 
-              10 LINE 01 COLUMN 15 PIC X(20)
-              BACKGROUND-COLOR 03 FOREGROUND-COLOR 0
-              FROM 'SISTEMAS DE CLIENTES'.
 
-              10  LINE 02 COLUMN 01 PIC X(25) ERASE EOL
-              BACKGROUND-COLOR 01 FROM WRK-MODULO.
-
-       01  MENU.
-           05 LINE 07 COLUMN 15 VALUE '1 - INCLUIR'.
-           05 LINE 08 COLUMN 15 VALUE '2 - CONSULTAR'.
-           05 LINE 09 COLUMN 15 VALUE '3 - ALTERAR'.
-           05 LINE 10 COLUMN 15 VALUE '4 - DELETAR'.
-           05 LINE 11 COLUMN 15 VALUE '5 - RELATORIO'.
-           05 LINE 12 COLUMN 15 VALUE 'X - SAIDA'.
-           05 LINE 13 COLUMN 15 VALUE 'OPCAO...: '.
-           05 LINE 13 COLUMN 28 USING WRK-OPCAO.
+       01 TELA-REGISTRO.
+           05 CHAVE FOREGROUND-COLOR 2.
+             10 LINE 10 COLUMN 10 VALUE 'ID: '.
+             10 COLUMN PLUS 2 PIC 9(4) USING CLIENTES-ID
+                 BLANK WHEN ZEROS.
+           05 SS-DADOS.
+             10 LINE 11 COLUMN 10 VALUE 'NOME...: '.
+             10 COLUMN PLUS 2 PIC X(30) USING CLIENTES-NOME.
+             10 LINE 12 COLUMN 10 VALUE 'EMAIL...: '.
+             10 COLUMN PLUS 2 PIC X(40) USING CLIENTES-EMAIL.
 
 
        PROCEDURE DIVISION.
        0001-PRINCIPAL SECTION.
-           PERFORM 1000-INICIAR.
-           PERFORM 2000-PROCESSAR.
-           PERFORM 3000-FINALIZAR.
-
-           STOP RUN.
+            PERFORM 1000-INICIAR.
+            PERFORM 2000-PROCESSAR.
+            PERFORM 3000-FINALIZAR.
+            STOP RUN.
 
        1000-INICIAR.
-           DISPLAY TELA.
+           OPEN I-O CLIENTES
 
-           ACCEPT MENU.
+           IF CLIENTES-STATUS = 35 THEN
+             OPEN OUTPUT CLIENTES
+             CLOSE CLIENTES
+             OPEN I-O CLIENTES
+
+           END-IF.
+
+
 
        2000-PROCESSAR.
+            EVALUATE WRK-OPCAO
+              WHEN 1
+               PERFORM 5000-INCLUIR
+              WHEN 2
+                CONTINUE
+              WHEN 3
+                CONTINUE
+              WHEN 4
+                CONTINUE
+              WHEN 5
+                CONTINUE
+              WHEN OTHER
+                IF WRK-OPCAO NOT EQUAL 'X'
+                    DISPLAY 'ENTRE COM OPCAO CORRETA'
+                END-IF
+            END-EVALUATE.
 
-           EVALUATE WRK-OPCAO
 
-           WHEN 1
-           PERFORM 5000-INCLUIR
 
-           WHEN 2
-           CONTINUE
 
-           WHEN 3
-           CONTINUE
+       3000-FINALIZAR.
+             CLOSE CLIENTES.
 
-           WHEN 4
-           CONTINUE
-
-           WHEN 5
-           CONTINUE
-
-           WHEN OTHER
-
-           IF WRK-OPCAO NOT EQUAL 'X'
-           DISPLAY 'ENTRE COM A OPCAO CORRETA'
-           END-IF
-           END-EVALUATE.
 
 
        5000-INCLUIR.
-           MOVE '- MODULO DE INCLUSAO' TO WRK-MODULO.
-           DISPLAY TELA.
-           DISPLAY WRK-TECLA  AT 1620.
+             MOVE 'MODULO - INCLUSAO ' TO WRK-MODULO.
+             DISPLAY TELA.
+             ACCEPT TELA-REGISTRO.
+              WRITE CLIENTES-REG
+                 IF CLIENTES-STATUS = 22
+                   DISPLAY 'REGISTRO JA EXISTE.'
+                    ACCEPT WRK-OPCAO
+                  END-IF.
 
-       3000-FINALIZAR.
-           CONTINUE.
+               WRITE CLIENTES-REG.
+
+            DISPLAY TELA.
+            ACCEPT MENU.
